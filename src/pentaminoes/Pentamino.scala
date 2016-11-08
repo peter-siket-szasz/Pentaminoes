a4e22588f2275d649d587136fb48b3628cca3a96
Unknown <miroaurela@gmail.com>
2016-11-08 16:49:47 +0200

    Two methods and Pentamino object added to Pentamino class

--- src/pentaminoes/Pentamino.scala
+++ src/pentaminoes/Pentamino.scala
@@ -2,4 +2,25 @@ package pentaminoes
 
 class Pentamino {
   
+  def spinClockwise() = {
+    ???
+  }
+  
+  def spinCounterClocwise() = {
+    ???
+  }
+  
+
 }
+
+object Pentamino {
+  
+  def newP(colors: Array[Int]) = {
+    ???
+  }
+  
+  
+}
+
+// P, X, F, V, W, Y, I, T, Z, U, N, L
+
