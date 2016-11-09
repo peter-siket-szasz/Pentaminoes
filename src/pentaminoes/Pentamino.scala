0102a333ae33366fe7adf0d13368b13b85c3b26a
Unknown <miroaurela@gmail.com>
2016-11-09 22:05:55 +0200

    Major improvement in Pentamino class
    Added methods rotateClockwise(), rotateteCounterClockwise() and randomRotation.
    Changed return type of flipVertical and flipHorizontal from Unit to Pentamino.
    
    Added method for creating Pentamino by char and for creating random Pentamino (random shape and rotation) to companion object.

--- src/pentaminoes/Pentamino.scala
+++ src/pentaminoes/Pentamino.scala
@@ -1,8 +1,10 @@
 package pentaminoes
 
-class Pentamino(var array: Array[Array[Int]]) {
+import scala.util.Random
+
+class Pentamino(private var array: Array[Array[Int]]) {
   
-  override def toString = {
+  override def toString: String = {
     var text = ""
     for (i <- Range(0,5)) {
       text += this.array(i).mkString(" ") + "\n"
@@ -10,40 +12,101 @@ class Pentamino(var array: Array[Array[Int]]) {
     text
   }
   
-  def spinClockwise() = {
-    ???
+  def toVector: Vector[Vector[Int]] = this.array.map(_.toVector).toVector
+  
+  def apply(x: Int, y:Int):Int = this.toVector(x+2)(y+2) //relative, (2,2) -> (0,0)
+  
+  // rotate methods and flip methods and randomRotation all 
+  // both makes changes to original Pentamino and return the modified version of it
+  
+  def rotateClockwise(): Pentamino = {
+    val newArray = Array.fill(5,5)(0)
+    for (x <- Range(0,5); y <- Range(0,5)) newArray(x)(y) = this.array(4-y)(x)
+    this.array = newArray
+    this
   }
   
-  def spinCounterClocwise() = {
-    ???
+  def rotateCounterClockwise(): Pentamino = {
+    val newArray = Array.fill(5,5)(0)
+    for (x <- Range(0,5); y <- Range(0,5)) newArray(x)(y) = this.array(y)(4-x)
+    this.array = newArray
+    this
   }
   
-  def flip() = { // y-axis reflection
-    ???
+  def flipVertical(): Pentamino = { // x-axis reflection
+    val newArray = Array.fill(5,5)(0)
+    for (x <- Range(0,5); y <- Range(0,5)) newArray(x)(y) = this.array(4-x)(y)
+    this.array = newArray
+    this
   }
+  
+  def flipHorizontal(): Pentamino = { // y-axis reflection
+    val newArray = Array.fill(5,5)(0)
+    for (x <- Range(0,5); y <- Range(0,5)) newArray(x)(y) = this.array(x)(4-y)
+    this.array = newArray
+    this
+  }
+  
+  def randomRotation(): Pentamino = { // Randomly flip AND/OR rotate Pentamino
+    new Random().nextInt(4) match { // Random rotation
+      case 0 => this
+      case 1 => this.rotateClockwise()
+      case 2 => {this.rotateClockwise(); this.rotateClockwise()}
+      case 3 => this.rotateCounterClockwise()
+    }
     
+    new Random().nextInt(4) match { // Random flip
+      case 0 => this
+      case 1 => this.flipHorizontal()
+      case 2 => this.flipVertical()
+      case 3 => {this.flipHorizontal(); this.flipVertical()}
+    }
+
+    this
+  }
+  
   
 }
 
 object Pentamino {
   
-  def pentaminoes = Array("P", "X", "F", "V", "W", "Y", "I", "T", "Z", "U", "N", "L")
+  def pentaminoes: Array[Char] = Array('p', 'x', 'f', 'v', 'w', 'y', 'i', 't', 'z', 'u', 'n', 'l')
   
-  def apply(param: Array[Array[Int]]) = new Pentamino(param)
+  def apply(param: Array[Array[Int]]): Pentamino = new Pentamino(param)
   
-  def apply(list: Array[Int]) = { // cleaner solution?
-    new Pentamino(Array(Array(list(0), list(1), list(2), list(3), list(4)),
-                        Array(list(5), list(6), list(7), list(8), list(9)),
-                        Array(list(10), list(11), list(12), list(13), list(14)),
-                        Array(list(15), list(16), list(17), list(18), list(19)),
-                        Array(list(20), list(21), list(22), list(23), list(24))))
+  def apply(list: Array[Int]): Pentamino = {
+    val shape = Array.ofDim[Int](5,5)
+    for (i <- 0 until list.size) {
+      shape(i / 5)(i % 5) = list(i)
+    }
+    Pentamino(shape)
   }
   
-  def apply(list: Array[Array[Int]], char: Char) = {
-    ???
+  def apply(char: Char, c1: Int, c2: Int, c3: Int, c4: Int, c5: Int): Pentamino =
+    char match {
+      case 'p' | 'P' => this.p(c1, c2, c3, c4, c5)
+      case 'x' | 'X' => this.x(c1, c2, c3, c4, c5)
+      case 'f' | 'F' => this.f(c1, c2, c3, c4, c5)
+      case 'v' | 'V' => this.v(c1, c2, c3, c4, c5)
+      case 'w' | 'W' => this.w(c1, c2, c3, c4, c5)
+      case 'y' | 'Y' => this.y(c1, c2, c3, c4, c5)
+      case 'i' | 'I' => this.i(c1, c2, c3, c4, c5)
+      case 't' | 'T' => this.t(c1, c2, c3, c4, c5)
+      case 'z' | 'Z' => this.z(c1, c2, c3, c4, c5)
+      case 'u' | 'U' => this.u(c1, c2, c3, c4, c5)
+      case 'n' | 'N' => this.n(c1, c2, c3, c4, c5)
+      case 'l' | 'L' => this.l(c1, c2, c3, c4, c5)
+      case  _  => this(Array.ofDim[Int](5,5)) // Default = empty "Pentamino"
+    }
+  
+  // Random shaped and rotated Pentamino
+  def random(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
+    val randomInt = new Random().nextInt(pentaminoes.size)
+    val pentaminoChar = this.pentaminoes(randomInt)
+    this(pentaminoChar, c1, c2, c3, c4, c5).randomRotation()
   }
   
-  def p(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def p(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0, 0, 0,
                       0, 0, c1,c2,0,
                       0, 0, c3,c4,0,
@@ -51,92 +114,92 @@ object Pentamino {
                       0, 0, 0, 0, 0) )
   }
   
-  def x(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def x(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0, 0, 0,
-                          0, 0, c1,0, 0,
-                          0, c2,c3,c4,0,
-                          0, 0, c5,0, 0,
-                          0, 0, 0, 0, 0) )
+                      0, 0, c1,0, 0,
+                      0, c2,c3,c4,0,
+                      0, 0, c5,0, 0,
+                      0, 0, 0, 0, 0) )
   }
   
-  def f(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def f(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0, 0, 0,
-                          0, c1,c2,0, 0,
-                          0, 0 ,c3,c4,0,
-                          0, 0, c5,0, 0,
-                          0, 0, 0, 0, 0) )
+                      0, c1,c2,0, 0,
+                      0, 0 ,c3,c4,0,
+                      0, 0, c5,0, 0,
+                      0, 0, 0, 0, 0) )
   }
   
-  def v(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def v(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0, 0, 0,
-                          0, 0, 0 ,0, 0,
-                          0, 0, c1,c2,c3,
-                          0, 0, c4,0, 0,
-                          0, 0, c5,0, 0) )
+                      0, 0, 0 ,0, 0,
+                      0, 0, c1,c2,c3,
+                      0, 0, c4,0, 0,
+                      0, 0, c5,0, 0) )
   }
   
-  def w(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def w(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0, 0, 0,
-                          0, c1,0 ,0, 0,
-                          0, c2,c3,0, 0,
-                          0, 0, c4,c5,0,
-                          0, 0, 0, 0, 0) )
+                      0, c1,0 ,0, 0,
+                      0, c2,c3,0, 0,
+                      0, 0, c4,c5,0,
+                      0, 0, 0, 0, 0) )
   }
   
-  def y(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def y(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0, 0, 0,
-                          0, 0, c1,0, 0,
-                          0, 0, c2,c3,0,
-                          0, 0, c4,0, 0,
-                          0, 0, c5,0, 0) )
+                      0, 0, c1,0, 0,
+                      0, 0, c2,c3,0,
+                      0, 0, c4,0, 0,
+                      0, 0, c5,0, 0) )
   }
   
-  def i(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def i(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, c1,0, 0,
-                          0, 0, c2,0, 0,
-                          0, 0, c3,0, 0,
-                          0, 0, c4,0, 0,
-                          0, 0, c5,0, 0) )
+                      0, 0, c2,0, 0,
+                      0, 0, c3,0, 0,
+                      0, 0, c4,0, 0,
+                      0, 0, c5,0, 0) )
   }
   
-  def t(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def t(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0 ,0, 0,
-                          0, c1,c2,c3,0,
-                          0, 0, c4,0, 0,
-                          0, 0, c5,0, 0,
-                          0, 0, 0, 0, 0) )
+                      0, c1,c2,c3,0,
+                      0, 0, c4,0, 0,
+                      0, 0, c5,0, 0,
+                      0, 0, 0, 0, 0) )
   }
   
-  def z(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def z(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0 ,0, 0,
-                          0, c1,c2,0, 0,
-                          0, 0, c3,0, 0,
-                          0, 0, c4,c5,0,
-                          0, 0, 0, 0, 0) )
+                      0, c1,c2,0, 0,
+                      0, 0, c3,0, 0,
+                      0, 0, c4,c5,0,
+                      0, 0, 0, 0, 0) )
   }
   
-  def u(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def u(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0 ,0, 0,
-                          0, c1,0, c2,0,
-                          0, c3,c4,c5,0,
-                          0, 0, 0 ,0, 0,
-                          0, 0, 0, 0, 0) )
+                      0, c1,0, c2,0,
+                      0, c3,c4,c5,0,
+                      0, 0, 0 ,0, 0,
+                      0, 0, 0, 0, 0) )
   }
   
-  def n(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def n(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0 ,0, 0,
-                          0, 0, 0, c1,0,
-                          0, 0, c2,c3,0,
-                          0, 0, c4,0, 0,
-                          0, 0, c5,0, 0) )
+                      0, 0, 0, c1,0,
+                      0, 0, c2,c3,0,
+                      0, 0, c4,0, 0,
+                      0, 0, c5,0, 0) )
   }
   
-  def l(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int) = {
+  def l(c1: Int, c2: Int, c3:Int, c4:Int, c5:Int): Pentamino = {
     Pentamino( Array( 0, 0, 0 ,0, 0,
-                          0, 0, c1,c2,0,
-                          0, 0, c3,0, 0,
-                          0, 0, c4,0, 0,
-                          0, 0, c5,0, 0) )
+                      0, 0, c1,c2,0,
+                      0, 0, c3,0, 0,
+                      0, 0, c4,0, 0,
+                      0, 0, c5,0, 0) )
   }
   
 }
--- src/pentaminoes/Tester.scala
+++ src/pentaminoes/Tester.scala
@@ -2,6 +2,7 @@ package pentaminoes
 
 object Tester extends App{
   
+  /*
   println(Pentamino.p(1,1,1,1,1).toString)
   println(Pentamino.x(1,1,1,1,1).toString)
   println(Pentamino.f(1,1,1,1,1).toString)
@@ -14,5 +15,43 @@ object Tester extends App{
   println(Pentamino.u(1,1,1,1,1).toString)
   println(Pentamino.n(1,1,1,1,1).toString)
   println(Pentamino.l(1,1,1,1,1).toString)
+  // */
   
+  /*
+  val pentaminoF = Pentamino.f(1,2,3,4,5)
+  println(pentaminoF)
+  pentaminoF.flipHorizontal()
+  println(pentaminoF)
+  pentaminoF.flipVertical()
+  println(pentaminoF)
+  pentaminoF.spinCounterClockwise()
+  println(pentaminoF)
+  pentaminoF.spinClockwise()
+  println(pentaminoF)
+  // */
+  
+  /*
+  println(Pentamino.i(1,2,3,4,5)(0,0))
+  println(Pentamino.i(1,2,3,4,5)(-1,2))
+  */
+
+  /*
+	println(Pentamino('i',1,2,3,4,5))
+	println(Pentamino('I',1,2,3,4,5))
+	println(Pentamino('p',1,2,3,4,5))
+	println(Pentamino('ä',1,2,3,4,5))
+	// */
+
+	val list = Array.fill(24)(Pentamino.random(1, 2, 3, 4, 5))
+	println(list.mkString("\n"))
+	
+	
+	
+	
+	
+	
+	
+	
+
+
 }
